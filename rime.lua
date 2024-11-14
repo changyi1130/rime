6d0d24d9cfd6e7338abfd3d93580d7398284df32
Changyi <fuhailong_2023@qq.com>
2024-11-14 14:15:26 +0800

    添加time，临时英文

--- rime.lua
+++ rime.lua
@@ -33,6 +33,12 @@ function date_translator(input, seg)
       --]]
       yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "日期"))
    end
+
+   -- 时间
+   if (input == "time") then
+      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), "时间"))
+      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), "时间"))
+   end
 end
 
 -- 将上述定义导出
